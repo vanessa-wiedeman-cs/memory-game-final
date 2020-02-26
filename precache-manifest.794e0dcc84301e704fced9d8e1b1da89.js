414cff9f6e50a76a07dcef5f4341b9f251b4fc5b
Vanessa <vanessa.wiedeman.cs@gmail.com>
2020-02-25 21:11:24 -0600

    Updates

---
+++ precache-manifest.794e0dcc84301e704fced9d8e1b1da89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8b9765f292ed9539d1537c56c9fb74",
+    "url": "/memory-game-final/index.html"
+  },
+  {
+    "revision": "24e4bcc253f403d35cc2",
+    "url": "/memory-game-final/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50059238eb72c25930cd",
+    "url": "/memory-game-final/static/js/2.60b78736.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory-game-final/static/js/2.60b78736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e4bcc253f403d35cc2",
+    "url": "/memory-game-final/static/js/main.e7f6061f.chunk.js"
+  },
+  {
+    "revision": "f16f4d8a83656c169531",
+    "url": "/memory-game-final/static/js/runtime-main.e1cb3195.js"
+  }
+]);
